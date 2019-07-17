510caef30967d96c794274b20c53d73ffac0ec83
kentaro.mimata <kentaro.mimata@rakus.co.jp>
2019-07-17 09:42:07 +0900

    tomcatで起動出来るように設定

--- src/main/java/com/example/demo/PipelineProjectApplication.java
+++ src/main/java/com/example/demo/PipelineProjectApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PipelineProjectApplication {
+public class PipelineProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PipelineProjectApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PipelineProjectApplication.class);
+	}
+	
 }
